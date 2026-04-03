b76d9e51538b0768d5305bf1621bc7980de4ed7d
Vinay Kumrawat <vinaykumrwat001@gmail.com>
2026-04-04 01:35:27 +0530

    hald backend improve push

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import connectDB from './src/database/db.js'
 import app from './src/app.js';
-dotenv.config({path:'./.env'});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenv.config({ path: join(__dirname, '.env') });
 
 const connection = async ()=>{
     try {
